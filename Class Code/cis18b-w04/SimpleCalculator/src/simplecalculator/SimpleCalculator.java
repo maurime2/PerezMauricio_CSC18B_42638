cc949f90df2f0310f07dfaebea295305ba4d74b4
Mauricio S. Perez <Maurime2@gmail.com>
2017-03-16 03:42:13 -0700

    Signed-off-by: Mauricio S. Perez <Maurime2@gmail.com>

---
+++ Class Code/cis18b-w04/SimpleCalculator/src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,12 @@
+package simplecalculator;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        //
+        CalculatorFrame myFrame = new CalculatorFrame();
+        myFrame.setVisible(true);
+        // you can also enable
+    }
+    
+}
